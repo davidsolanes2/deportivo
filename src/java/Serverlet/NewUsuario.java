77ddf7892870f2f5165a8a098016a240b036ba81
davidsolanes2 <davidsolanes2@gmail.com>
2017-04-14 21:49:07 +0200

    Deportivo v 1.5

--- src/java/Serverlet/NewUsuario.java
+++ src/java/Serverlet/NewUsuario.java
@@ -5,6 +5,11 @@ import Beans.DeportivoEJB;
 import Entities.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -30,7 +35,7 @@ public class NewUsuario extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ParseException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -41,9 +46,20 @@ public class NewUsuario extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             String a = request.getParameter("nombre");
+            String b = request.getParameter("apellidos");
+            String c = request.getParameter("telefono");
+            String d = request.getParameter("idnif");
+            String e = request.getParameter("direccion");
+            String f = request.getParameter("poblacion");
+            String g = request.getParameter("cp");
+            String h = request.getParameter("fechaAlta");
+            String i = request.getParameter("email");
+            
+                SimpleDateFormat j = new SimpleDateFormat("dd-MM-yyyy");
+                Date fechaAlta = j.parse(h);
             
             
-            Usuario u = new Usuario (a);
+            Usuario u = new Usuario (a, b, c, d, e, f, g, fechaAlta, i);
             if (miEjb.insertUsuario(u)) {
                 out.println("Usuario dado de alta");
             }
@@ -69,7 +85,11 @@ public class NewUsuario extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(NewUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -83,7 +103,11 @@ public class NewUsuario extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(NewUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
